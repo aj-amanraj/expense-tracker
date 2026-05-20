a6ea13825572628c51beec348c1503c0081edae7
Aman Raj <aman2807aj@gmail.com>
2026-05-21 01:13:29 +0530

    feat: Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-400 min-h-screen '>
+      <h1>Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default App
