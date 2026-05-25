788aa91041fdde22011cf96c3e2028d2c3fe1584
Aman Raj <aman2807aj@gmail.com>
2026-05-25 19:01:37 +0530

    fix: buid error

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { route } from "./route.js";
-import mongoDb from "./config/mongodb.js";
+import mongoDb from "./config/mongoDb.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;      // Use PORT from .env or fallback to 8080
+const port = process.env.PORT || 8080; // Use PORT from .env or fallback to 8080
 
-app.use(cors({origin: '*'}));   // allow cross-origin requests for all origins
+app.use(cors({ origin: "*" })); // allow cross-origin requests for all origins
 
-app.use(express.json());    // parse JSON request body into object
+app.use(express.json()); // parse JSON request body into object
 
-app.use(express.urlencoded({ extended: true}))    // parse form data
+app.use(express.urlencoded({ extended: true })); // parse form data
 
 mongoDb();
-route(app);     // function registers all application routes with the Express app
+route(app); // function registers all application routes with the Express app
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+  console.log(`server running on port ${port}`);
+});
