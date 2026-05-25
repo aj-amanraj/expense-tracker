a45c25db9d8a15ff3fe3a10bdd2c908edddd04c6
Aman Raj <aman2807aj@gmail.com>
2026-05-25 19:03:02 +0530

    feat: backend service uri

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,19 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = "http://localhost:3000/auth";
+const API_URL = "https://expense-tracker-2m7m.onrender.com/auth";
 
 export const signUpUser = async (userData) => {
-    const response = await axios.post(
-        `${API_URL}/signup`, userData
-    );
+  const response = await axios.post(`${API_URL}/signup`, userData);
 
-    return response.data;
+  return response.data;
 };
 
 export const signInUser = async (userData) => {
-    const response = await axios.post(
-        `${API_URL}/signin`, userData
-    );
+  const response = await axios.post(`${API_URL}/signin`, userData);
 
-    return response.data;
+  return response.data;
 };
--- client/src/utils/defaultApi.js
+++ client/src/utils/defaultApi.js
@@ -1,11 +1,12 @@
-
-import axios from 'axios'
+import axios from "axios";
 
 /**
  * create API service with the given URL
  */
 
-export const defaultApi = axios.create({baseURL: 'http://localhost:3000'});
+export const defaultApi = axios.create({
+  baseURL: "https://expense-tracker-2m7m.onrender.com",
+});
 
 /**
  * middleware that runs before request to the backend service
@@ -13,33 +14,33 @@ export const defaultApi = axios.create({baseURL: 'http://localhost:3000'});
  */
 
 defaultApi.interceptors.request.use(
-    async (config) => {
-        const token = await localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  async (config) => {
+    const token = await localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 /**
  * middleware that checks the request before resolving the promise
  */
 
 defaultApi.interceptors.response.use(
-    (response) => {
-        return response.data;
-    },
-    (error) => {
-        if (error.response && error.response.status === 401){
-            if(typeof window !== 'undefined'){
-                localStorage.removeItem('token')
-                window.location.href = '/signin';
-            }
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        window.location.href = "/signin";
+      }
     }
-)
+    return Promise.reject(error);
+  },
+);
