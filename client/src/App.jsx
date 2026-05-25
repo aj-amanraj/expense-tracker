53911735ff08edab11c09668733a220ca46f01d1
Aman Raj <aman2807aj@gmail.com>
2026-05-25 19:00:29 +0530

    fix: buid error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,46 @@
 import React from "react";
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/publicRoute";
+import PublicRoute from "./routes/PublicRoute";
 import NotFound from "./pages/NotFound";
-
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to='/signin' />}
-       />
+      <Route path="/" element={<Navigate to="/signin" />} />
 
-      <Route path="/signin" element={
-        <PublicRoute>
-          <SignIn />
-        </PublicRoute>
-      } />
-
-      <Route path="/signup" element={
-        <PublicRoute>
-          <SignUp />
-        </PublicRoute>
-      } />
-
-      <Route path="/dashboard" 
+      <Route
+        path="/signin"
         element={
-        <ProtectedRoute>
-          <Dashboard />
-        </ProtectedRoute>
-        } />
+          <PublicRoute>
+            <SignIn />
+          </PublicRoute>
+        }
+      />
 
-        <Route path="*" 
-          element={<NotFound />} 
-        />
+      <Route
+        path="/signup"
+        element={
+          <PublicRoute>
+            <SignUp />
+          </PublicRoute>
+        }
+      />
 
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
-    
   );
 };
 
